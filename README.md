ba5008656d0f290d369da93cdfa35aa20eb07a82
Shawn Keech <cshawnkeech@gmail.com>
2017-09-10 15:00:13 -0500

    Update 2

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Asteroids with p5js Tutorial
 
-Following coding challeng #46 with Daniel Schiffman on the coding train
+Following coding challenge #46 with Daniel Schiffman on the coding train
+
+
+windowWidth & windowHeight not working properly with createCanvas...
--- sketch.js
+++ sketch.js
@@ -1,12 +1,42 @@
+var ship;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas (600, 400);
+  ship = new Ship;
 }
 
 function draw() {
   background(0);
+  ship.render();
+
+
+
+
+}
+
+function keyPressed() {
+  if (keyCode == RIGHT_ARROW) {
+    ship.turn(0.1);
+  } else if (keyCode == LEFT_ARROW) {
+    ship.turn(-0.1);
+  }
 }
 
 function Ship() {
   this.pos = createVector(width/2, height/2);
-  
+  this.r = 20;
+  this.heading = 0;
+  noFill();
+  stroke(255);
+
+  this.render = function() {
+    translate(this.pos.x, this.pos.y);
+    rotate(this.heading);
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+  }
+
+  this.turn = function(angle) {
+    this.heading += angle;
+  }
+
 }
