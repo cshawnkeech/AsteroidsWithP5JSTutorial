29cfbf40fd9167cb98843038d4a9034f4feb8f37
Shawn Keech <cshawnkeech@gmail.com>
2017-09-10 13:40:13 -0500

    First work

---
+++ README.md
@@ -0,0 +1,3 @@
+# Asteroids with p5js Tutorial
+
+Following coding challeng #46 with Daniel Schiffman on the coding train
---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+}
+
+function Ship() {
+  this.pos = createVector(width/2, height/2);
+  
+}
